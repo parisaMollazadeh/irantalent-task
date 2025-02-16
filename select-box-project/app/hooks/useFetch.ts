16a0b8fd1202e0a5c555bf3e2899e067d801a93c
parisaMollazadeh <mollazadehparisa22@gmail.com>
2025-02-16 14:53:38 +0330

    create fetch hook

---
+++ select-box-project/app/hooks/useFetch.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { API_BASE_URL } from '../utils/api';
+
+// Define a generic type
+const useFetch = <T>(endpoint: string) => {
+  const [data, setData] = useState<T | null>(null);  
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${API_BASE_URL}${endpoint}`);
+        const result: T = await res.json(); // Type the result to T
+        setData(result);
+      } catch (err) {
+        setError('Failed to fetch data');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [endpoint]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
